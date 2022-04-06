4cac946d8bfa8c87fbdf8b93f5eea9da636cbecc
MS Nawaz <msnawazbd@gmail.com>
2022-04-07 04:17:19 +0600

    Added Invoice List

--- app/Http/Livewire/Admin/Invoices/ListInvoices.php
+++ app/Http/Livewire/Admin/Invoices/ListInvoices.php
@@ -7,8 +7,15 @@ use Livewire\Component;
 
 class ListInvoices extends Component
 {
+    public $status = null;
+
     public function render()
     {
+        $invoicesCount = Invoice::query()->count();
+        $paidInvoicesCount = Invoice::query()->where('status', 1)->count();
+        $partialPaidInvoicesCount = Invoice::query()->where('status', 2)->count();
+        $dueInvoicesCount = Invoice::query()->where('status', 3)->count();
+
         $invoices = Invoice::query()
             ->with([
                 'client'
@@ -17,7 +24,11 @@ class ListInvoices extends Component
             ->paginate(5);
 
         return view('livewire.admin.invoices.list-invoices', [
-            'invoices' => $invoices
+            'invoicesCount' => $invoicesCount,
+            'paidInvoicesCount' => $paidInvoicesCount,
+            'partialPaidInvoicesCount' => $partialPaidInvoicesCount,
+            'dueInvoicesCount' => $dueInvoicesCount,
+            'invoices' => $invoices,
         ]);
     }
 }
